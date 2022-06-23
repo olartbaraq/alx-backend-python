00b843a30a98d0de50ba3f8f168ff246e6c038a5
olart <akanbi.ges201@gmail.com>
2022-06-23 21:57:42 +0100

    advanced task 2

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""More involved type annotations"""
+
+from typing import Mapping, Any, Union, TypeVar
+"""necessary import for arguments and return values"""
+
+T = TypeVar('T')
+"""Decorator functions can be expressed via generics"""
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]
+                     ) -> Union[Any, T]:
+    """return type annotations to the function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
